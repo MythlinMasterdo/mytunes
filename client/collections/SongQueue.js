f61f8828cab3b8d05273e80eb2fe1c879e6fd252
MythlinMasterdo <miles71397@gmail.com>
2016-09-28 19:57:41 -0700

    Implemented SongModel and most of SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,37 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
+
+
   initialize: function() {
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      //Dequeue
+      this.trigger('dequeue', function() {
+        this.model[0];
+      });
+      if (this.length !== 1) {
+        this.playFirst();
+      }
+      //playFirst
+    });
+
+    this.on('dequeue', function(song) {
+      // debugger;
+      this.remove(song.cid);
+      // this.models.shift();
+    }, this);
+    console.log('remove', this.model[0]);
+  },
+
+
+  playFirst: function() {
+
   }
 
 });
